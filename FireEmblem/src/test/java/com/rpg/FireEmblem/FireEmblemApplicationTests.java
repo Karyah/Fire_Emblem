e66499bb6e0411b9002c3e4f831c90d9aee0474e
Karyah <mastrogiacomokaren@gmail.com>
2023-09-05 08:54:22 -0300

    Criando projeto

---
+++ FireEmblem/src/test/java/com/rpg/FireEmblem/FireEmblemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpg.FireEmblem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FireEmblemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
